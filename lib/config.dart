aada915b6ed132d7c3e6260416e44055f9edc010
azadlinavay <azadlinavay@hotmail.com>
2022-01-19 17:35:35 +0300

    Finished!

--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,83 @@
-const String linktree = "https://";
-const String bio =
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+import 'package:flutter/cupertino.dart';
+
+class User {
+  String? bio;
+  String? name;
+  String? imageLink;
+  String? location;
+  String? job;
+  List<SocialMedia>? socialMedia;
+  List<Contacts>? contacts;
+
+
+
+
+
+User({
+  this.job,
+  this.name,
+  this.imageLink,
+  this.location,
+  this.bio,
+  this.socialMedia,
+  this.contacts,
+
+});
+
+
+}
+
+class SocialMedia {
+  String? name;
+  String? link;
+  String? imageName;
+
+  SocialMedia({
+    this.name,
+    this.link,
+    this.imageName,
+  });
+
+}
+
+class Contacts {
+  String? name;
+  String? link;
+  String? imageName;
+
+
+
+
+
+Contacts({
+  this.name,
+  this.link,
+  this.imageName,
+});
+
+}
+
+
+
+User user = User(
+  job: "Devleoper",
+  name: "Azad Linavay",
+  bio: 'Ez Azad Linavay me',
+  imageLink: "azad.jpg",
+  location: "Iraq - Duhok",
+  socialMedia: [
+    SocialMedia(name: "LinkedIn", link: "https://www.linkedin.com", imageName: "linkedin.png"),
+    SocialMedia(name: "GitHub", link: "https://github.com/", imageName: "github.png"),
+    SocialMedia(name: "StackOverFlow", link: "https://stackoverflow.com/", imageName: "stack.png"),
+    SocialMedia(name: "Facebook", link: "https://www.facebook.com", imageName: "facebook.png"),
+    SocialMedia(name: "Instagram", link: "https://www.instagram.com", imageName: "instagram.png"),
+    SocialMedia(name: "Youtube", link: "https://www.youtube.com", imageName: "youtube.png"),
+    SocialMedia(name: "Snapchat", link: "https://www.snapchat.com", imageName: "snapchat.png"),
+  ],
+  contacts: [
+    Contacts(name: "WhatsApp", link: "https://wa.me/+9647507601194", imageName: "whatsapp.png"),
+    Contacts(name: "Viber", link: "viber://contact?number=+9647507601194", imageName: "viber.png"),
+    Contacts(name: "phone", link: "+9647507601194", imageName: "phone.png"),
+  ]
+);
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:linktree/config.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const linktree());
@@ -17,232 +19,178 @@ class _linktreeState extends State<linktree> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      
       home: Scaffold(
+        backgroundColor: Colors.grey[900],
         body: ListView(
-          children: [
-            Center(
-              child: Column(
-                children: [
-                  Container(
-                    decoration: const BoxDecoration(),
-                    margin: const EdgeInsets.only(top: 50),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20), // Image border
-                      child: SizedBox.fromSize(
-                        size: const Size.fromRadius(100), // Image radius
-                        child:
-                            Image.asset('images/azad.jpg', fit: BoxFit.cover),
-                      ),
+        children: [
+          Center(
+            child: Column(
+              children: [
+                Container(
+                  decoration: const BoxDecoration(),
+                  margin: const EdgeInsets.only(top: 50),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20), // Image border
+                    child: SizedBox.fromSize(
+                      size: const Size.fromRadius(100), // Image radius
+                      child: Image.asset('images/${user.imageLink}',
+                          fit: BoxFit.cover),
                     ),
                   ),
-                  Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        color: Colors.black87),
-                    width: 200,
-                    height: 25,
-                    child: const Center(
-                        child: Text(
-                      "Looking For Job",
-                      style: TextStyle(color: Colors.white),
-                    )),
-                  ),
-                ],
-              ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: Colors.black87),
+                  width: 200,
+                  height: 25,
+                  child: Center(
+                      child: Text(
+                    user.job ?? "",
+                    style: const TextStyle(color: Colors.white),
+                  )),
+                ),
+              ],
             ),
-            Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                        color: Colors.blue[900],
-                        borderRadius: BorderRadius.circular(5)),
-                    margin: const EdgeInsets.all(4),
-                    child: const Text(
-                      "Azad Linavay",
-                      style: TextStyle(fontSize: 18, color: Colors.white),
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                        color: Colors.blue[900],
-                        borderRadius: BorderRadius.circular(5)),
-                    margin: const EdgeInsets.all(4),
-                    child: const Text(
-                      "Developer",
-                      style: TextStyle(fontSize: 18, color: Colors.white),
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                        color: Colors.blue[900],
-                        borderRadius: BorderRadius.circular(5)),
-                    margin: const EdgeInsets.all(4),
-                    child: const Text(
-                      "Iraq - Duhok",
-                      style: TextStyle(fontSize: 18, color: Colors.white),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Column(
+          ),
+          Center(
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
+                  padding: const EdgeInsets.all(10),
                   decoration: BoxDecoration(
-                      color: Colors.red[900],
+                      color: Colors.blue[900],
                       borderRadius: BorderRadius.circular(5)),
-                  padding: const EdgeInsets.all(4),
-                  margin: const EdgeInsets.all(10),
-                  height: 30,
-                  child: const Text(
-                    "Visit My Links",
-                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  margin: const EdgeInsets.all(4),
+                  child: Text(
+                    user.name ?? "No Name",
+                    style: TextStyle(fontSize: 18, color: Colors.white),
                   ),
                 ),
                 Container(
+                  padding: const EdgeInsets.all(10),
                   decoration: BoxDecoration(
-                      color: Colors.red[900],
+                      color: Colors.blue[900],
                       borderRadius: BorderRadius.circular(5)),
-                  margin: const EdgeInsets.all(10),
-                  height: 55,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                          padding: EdgeInsets.all(5),
-                          child: Image.asset("images/azad.jpg")),
-                      Text(
-                        "LinkedIn",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: Colors.red[900],
-                      borderRadius: BorderRadius.circular(5)),
-                  margin: const EdgeInsets.all(10),
-                  height: 55,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                          padding: EdgeInsets.all(5),
-                          child: Image.asset("images/azad.jpg")),
-                      Text(
-                        "GitHub",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: Colors.red[900],
-                      borderRadius: BorderRadius.circular(5)),
-                  margin: const EdgeInsets.all(10),
-                  height: 55,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                          padding: EdgeInsets.all(5),
-                          child: Image.asset("images/azad.jpg")),
-                      Text(
-                        "Website",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: Colors.red[900],
-                      borderRadius: BorderRadius.circular(5)),
-                  margin: const EdgeInsets.all(10),
-                  height: 55,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                          padding: EdgeInsets.all(5),
-                          child: Image.asset("images/azad.jpg")),
-                      Text(
-                        "WhatsApp",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: Colors.red[900],
-                      borderRadius: BorderRadius.circular(5)),
-                  margin: const EdgeInsets.all(10),
-                  height: 55,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                          padding: EdgeInsets.all(5),
-                          child: Image.asset("images/azad.jpg")),
-                      Text(
-                        "Instagram",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: Colors.red[900],
-                      borderRadius: BorderRadius.circular(5)),
-                  margin: const EdgeInsets.all(10),
-                  height: 55,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                          padding: EdgeInsets.all(5),
-                          child: Image.asset("images/azad.jpg")),
-                      Text(
-                        "Facebook",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: Colors.red[900],
-                      borderRadius: BorderRadius.circular(5)),
-                  margin: const EdgeInsets.all(10),
-                  height: 55,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                          padding: EdgeInsets.all(5),
-                          child: Image.asset("images/azad.jpg")),
-                      Text(
-                        "Viber",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ],
+                  margin: const EdgeInsets.all(4),
+                  child: Text(
+                    user.location ?? "",
+                    style: TextStyle(fontSize: 18, color: Colors.white),
                   ),
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+          Container(
+            height: 300,
+            child: ListView.builder(
+                itemCount: user.socialMedia?.length ?? 0,
+                itemBuilder: (context, index) {
+                  if (user.socialMedia?[index] == null) {
+                    return Text("data");
+                  }
+          
+                  SocialMedia s = user.socialMedia![index];
+                  return Container(
+                    margin: EdgeInsets.all(10),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.white,
+                        onPrimary: Colors.black54
+                      ),
+                      onPressed: () async {
+                        String link = s.link ?? "https://fb.com/azadlinavay";
+                        await _launchInBrowser(link);
+                      },
+                      child: Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            width: 50,
+                            height: 50,
+                            child: Image.asset("images/${s.imageName}"),
+                            
+                            ),
+                          Container(
+                            padding: EdgeInsets.all(15),
+                            child: Text(s.name ?? ""),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 20.0, bottom: 20),
+            height: 110.0,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: user.contacts?.length?? 0,
+              itemBuilder: (c,i){
+                if (user.contacts?[i] == null) {
+                    return Text("data");
+                  }
+                  Contacts contacts = user.contacts![i];
+                return Container(
+                  margin: EdgeInsets.all(5),
+                  width: 70,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(    shape: CircleBorder()
+                    ,
+                    primary: Colors.white
+
+                      ),
+                    onPressed: () async {
+
+if (user.contacts?[i].name == "phone") {
+
+  String alink = contacts.link ?? "+9947500000000";
+                        await _makePhoneCall(alink);
+} else {
+  String alink = contacts.link ?? "https://fb.com/azadlinavay";
+                        await _launchInBrowser(alink);
+}
+
+                        
+                      }, child: Image.asset('images/${contacts.imageName}'),),
+                );
+              }),
+              // This next line does the trick.
+              
+            
+          ),
+        ],
+        )
       ),
     );
+  }
+
+  Future<void> _launchInBrowser(String url) async {
+    if (!await launch(
+      url,
+      forceSafariVC: false,
+      forceWebView: false,
+      headers: <String, String>{'my_header_key': 'my_header_value'},
+    )) {
+      throw 'Could not launch $url';
+    }
+  }
+
+
+
+
+    Future<void> _makePhoneCall(String phoneNumber) async {
+    // Use `Uri` to ensure that `phoneNumber` is properly URL-encoded.
+    // Just using 'tel:$phoneNumber' would create invalid URLs in some cases,
+    // such as spaces in the input, which would cause `launch` to fail on some
+    // platforms.
+    final Uri launchUri = Uri(
+      scheme: 'tel',
+      path: phoneNumber,
+    );
+    await launch(launchUri.toString());
   }
 }
